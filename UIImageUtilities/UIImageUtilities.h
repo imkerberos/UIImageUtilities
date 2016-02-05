88f973bd54a125f615b19448167bd726158f25d2
Zhang Qigang <zhangqigang@letv.com>
2016-02-05 11:52:45 +0800

    add carthage support

---
+++ UIImageUtilities/UIImageUtilities.h
@@ -0,0 +1,23 @@
+//
+//  UIImageUtilities.h
+//  UIImageUtilities
+//
+//  Created by Kerberos Zhang on 16/2/5.
+//  Copyright © 2016年 Behaiku. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for UIImageUtilities.
+FOUNDATION_EXPORT double UIImageUtilitiesVersionNumber;
+
+//! Project version string for UIImageUtilities.
+FOUNDATION_EXPORT const unsigned char UIImageUtilitiesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UIImageUtilities/PublicHeader.h>
+
+#import <UIImageUtilities/UIImage+Blur.h>
+#import <UIImageUtilities/UIImage+fixOrientation.h>
+#import <UIImageUtilities/UIImage+Resize.h>
+#import <UIImageUtilities/UIImage+RoundCorners.h>
+#import <UIImageUtilities/UIImage+Tinted.h>
